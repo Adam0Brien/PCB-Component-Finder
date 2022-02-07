1f27c33c06c76c54685f5667783d9a9be9ae18e5
Adam0Brien <74919798+Adam0Brien@users.noreply.github.com>
2022-02-07 22:36:36 +0000

    Green PCBs can now turn black and white after selecting a green part of the board.
    sliders from JavaShop have also been implemented as well as displaying file details

--- src/main/java/adam0brien/pcbhelper/Controller.java
+++ src/main/java/adam0brien/pcbhelper/Controller.java
@@ -3,10 +3,10 @@ package adam0brien.pcbhelper;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.image.PixelFormat;
-import javafx.scene.image.WritableImage;
+import javafx.scene.control.Slider;
+import javafx.scene.effect.ColorAdjust;
+import javafx.scene.image.*;
+import javafx.scene.paint.Color;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
@@ -14,13 +14,20 @@ import java.io.File;
 import java.io.IOException;
 
 public class Controller {
+
     @FXML
     ImageView view;
-    public ImageView imageChosen;
-
-
+    @FXML
+    ImageView processedView;
     Stage stage;
-
+    @FXML
+    Label fileName;
+    @FXML
+    Label fileSize;
+    @FXML
+    Label fileName1;
+    @FXML
+    Label fileSize1;
 
     public void open(ActionEvent event) throws IOException {
         FileChooser fileChooser = new FileChooser();
@@ -30,38 +37,42 @@ public class Controller {
             file.toURI();
 
             Image image = new Image(file.toURI().toString(), view.getFitWidth(), view.getFitHeight(), false, true);
+
+            imagePicked = image;
+
+
+
             view.setImage(image);
+            processedView.setImage(image);
+
+            fileName.setText(file.getName());
+
+            fileSize.setText(file.length() + "KB");
+
+            fileName1.setText(file.getName());
+
+            fileSize1.setText(file.length() + "KB");
         }
 
     }
 
-
-
-    public void setGreyScale (ActionEvent event){
-        greyScale(view);
+    /**
+     * Black and white image conversion
+     *
+     * @param event
+     */
+    public void setBaW(ActionEvent event) {
+        BaW(processedView);
 
     }
 
-
-    public void greyScale (ImageView imageView) throws IndexOutOfBoundsException {
-
+    public void BaW(ImageView imageView) throws IndexOutOfBoundsException {
 
         try {
             //get image width and height
             int width = (int) imageView.getImage().getWidth();
             int height = (int) imageView.getImage().getHeight();
-            byte[] buffer = new byte[width * height * 4];
             WritableImage ii = new WritableImage(width, height);
-
-            imageView.getImage().getPixelReader().getPixels(0,
-                    0,
-                    width,
-                    height,
-                    PixelFormat.getByteBgraInstance(),
-                    buffer,
-                    0,
-                    width * 4);
-
 
             //convert to grayscale
             for (int y = 0; y < height; y++) {
@@ -71,42 +82,197 @@ public class Controller {
                     int a = (p >> 24) & 255;
                     int r = (p >> 16) & 255;
                     int g = (p >> 8) & 255;
-                    int b = p & 255;
-
+                    int b =  p & 255;
 
                     //calculate average
                     int avg = (r + g + b) / 3;
 
                     //replace RGB value with avg
-                    p = (a << 24) | (r << 16) | (g << 16) | b;   //make green pixels disappear
+                    p = (a << 24) | (avg << 16) | (avg << 8) | avg;
 
-
+                    //img.setRGB(x, y, p);
                     ii.getPixelWriter().setArgb(x, y, p);
-
                 }
             }
-            
             imageView.setImage(ii);
         } catch (Exception e) {
             System.out.println("Error: " + e);
         }
-    }
-
-
-
-    public void findResistors(){
-
-
-
-
 
     }
 
 
+    /**
+     * Sliders for Image Processing
+     */
+
+
+    @FXML
+    Slider hueSlider;
+
+    @FXML
+    Slider brightnessSlider;
+
+    @FXML
+    Slider saturationSlider;
+
+
+    ColorAdjust colorAdjust = new ColorAdjust();
+
+    public void hueChanger(double value) {
+        try {
+
+            colorAdjust.setHue(value);
+
+            processedView.setEffect(colorAdjust);
+
+
+        } catch (Exception e) {
+            System.out.println("Error:" + e);
+            System.out.println(hueSlider.getValue());
+        }
+
+    }
+
+    public void hueSlider() {
+        hueChanger(hueSlider.getValue());
+    }
+
+
+    public void brightnessChanger(double value) {
+
+        colorAdjust.setBrightness(value);
+
+        processedView.setEffect(colorAdjust);
+    }
+
+    public void brightnessSlider() {
+        brightnessChanger(brightnessSlider.getValue());
+    }
+
+
+    public void saturationChanger(double value) {
+
+        colorAdjust.setSaturation(value);
+        processedView.setEffect(colorAdjust);
+
+
+    }
+
+    public void saturationSlider() {
+        saturationChanger(saturationSlider.getValue());
+
+    }
+
+    public void reset(ActionEvent actionEvent) {
+
+        Image original = view.getImage();
+        processedView.setImage(original);
+        colorAdjust.setHue(0);
+        colorAdjust.setSaturation(0);
+        colorAdjust.setBrightness(0);
+
+        hueSlider.setValue(0);
+        saturationSlider.setValue(0);
+        brightnessSlider.setValue(0);
+    }
+
+
+    /**
+     * Component Finder
+     * <p>
+     * - This will find components of a specific color and change them to a more obvious color
+     * - The Background will then me removed be left with just the coloured components based on shape/size/original color
+     */
+
+    private Color selectedColor;
 
 
 
+    public void setSelectedColor(Color selectedColor) {
+        this.selectedColor = selectedColor;
+    }
+    public Color getSelectedColor() {
+        return selectedColor;
+    }
+    Image imagePicked;
+    WritableImage baW;
+
+
+    public void changeComponentColor(ActionEvent event) {
+        processedView.setImage(imagePicked);
+        brightnessSlider.getValue();
+        saturationSlider.getValue();
+
+        double finalHue = getSelectedColor().getHue();                                          //hue
+        double finalSat = getSelectedColor().getSaturation();          //saturation
+        double finalBri = getSelectedColor().getBrightness();       //brightness
+
+
+        PixelReader pixelReader = processedView.getImage().getPixelReader();
+        WritableImage baW = new WritableImage((int) processedView.getImage().getWidth(), (int) processedView.getImage().getHeight());
+        PixelWriter writer = baW.getPixelWriter();
+
+        Color black = new Color(0, 0, 0, 1);
+        Color white = new Color(1, 1, 1, 1);
+
+        //convert
+        for (int i = 0; i < imagePicked.getWidth(); i++) {
+            for (int j = 0; j < imagePicked.getHeight(); j++) {
+
+                Color oldColor = pixelReader.getColor(i, j);
+
+
+                //for green PCB's
+                if (oldColor.getGreen() <= oldColor.getRed() + oldColor.getBlue()) {
+                    writer.setColor(i, j, black);
+                }
+//                //for blue PCB's
+//                else if(oldColor.getBlue() <= oldColor.getGreen() + oldColor.getRed()) {
+//
+//                    writer.setColor(i,j,black);
+//                }
+//                //for red PBC's
+//                else if(oldColor.getRed() <= oldColor.getBlue()+ oldColor.getGreen()){
+//                    writer.setColor(i,j,black);
+//
+//                }
+            }
+
+
+                    //find average saturation hue and brightness for integrated circuits resistors LEDs and capacitors
+
+                    //use those values in a series of if statements to change the color of each component so they are easily recognisable
+
+
+                }
+
+                processedView.setImage(baW);
+                setBawImage(baW);
+
+
+    }
+
+
+    private void setBawImage(WritableImage baW) {
+        this.baW = baW;
+    }
 
 
 
+    public void selectColour(ActionEvent event) throws IOException {
+        //gets image colour
+
+        processedView.setOnMousePressed(e -> {
+            Color selectedColor = processedView.getImage().getPixelReader().getColor((int) e.getX(), (int) e.getY());
+            System.out.println("\nHue: " + selectedColor.getHue());
+            System.out.println("Saturation: " + selectedColor.getSaturation());
+            System.out.println("Brightness: " + selectedColor.getBrightness());
+            System.out.println("Red Value: " + selectedColor.getRed() * 255);
+            System.out.println("Green Value: " + selectedColor.getGreen() * 255);
+            System.out.println("Blue Value: " + selectedColor.getBlue() * 255);
+
+            setSelectedColor(selectedColor);
+        });
+    }
 }
--- src/main/java/adam0brien/pcbhelper/Driver.java
+++ src/main/java/adam0brien/pcbhelper/Driver.java
@@ -3,6 +3,7 @@ package adam0brien.pcbhelper;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,9 +12,10 @@ public class Driver extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Driver.class.getResource("pcbHelper.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 754, 554);
+        Scene scene = new Scene(fxmlLoader.load(), 925, 614);
         stage.setTitle("PCB Helper");
         stage.setScene(scene);
+        stage.getIcons().add(new Image("https://www.clipartmax.com/png/middle/133-1336612_vector-clip-art-of-rsa-electronics-capacitor-symbol-simbolo-de-un-resistor.png"));
         stage.show();
     }
 
